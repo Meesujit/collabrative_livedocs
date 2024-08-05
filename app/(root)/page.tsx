b60464eef2303630d655d5cbecee2a219f991c35
sujit gouda <sujit.gda997@gmail.com>
2024-08-05 19:42:10 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Button>button</Button>
+    </div>
+  )
+}
+
+export default Home
